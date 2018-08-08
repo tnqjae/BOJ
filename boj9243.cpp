c6c19412502241fe94ad835ddc2c8494398f1f25
SteffenLee <tnqjae@naver.com>
2018-08-08 17:07:42 +0900

    Add files via upload

---
+++ boj9243.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int N, flag = 0;
+string bf, af;
+
+
+int main() {
+	/*cin >> N;
+	cin >> bf;
+	cin >> af;
+
+	for (int i = 0; i < bf.size(); ++i) {
+		if ((bf.at(i) == af.at(i))^!(N&1)) {
+			cout << "Deletion failed" << endl;
+			flag = 1;
+			break;
+		}
+	}
+	if (flag == 0)
+		cout << "Deletion succeeded" << endl;*/
+	int a = 1;
+	cout << !a << endl;
+}
