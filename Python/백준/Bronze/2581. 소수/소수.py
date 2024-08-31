2d9888cca994fd7ab4f3e45dac2630e269c945cb
SteffenLee <tnqjae@naver.com>
2024-08-31 15:09:02 +0900

    [Bronze II] Title: 소수, Time: 512 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2581. 소수/소수.py
@@ -0,0 +1,18 @@
+m = int(input())
+n = int(input())
+prime = []
+for i in range(m, n+1):
+    cnt = 0
+    if i > 1:
+        for j in range(2, i):
+            if i % j == 0:
+                cnt += 1
+                break
+        if cnt == 0:
+            prime.append(i)
+            
+if len(prime) > 0:
+    print(sum(prime))
+    print(min(prime))
+else:
+    print(-1)
